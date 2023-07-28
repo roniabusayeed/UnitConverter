b23a90b80571ff143d3f0f8a03347f3484c4c9d4
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-07-28 14:22:33 -0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Abu Sayeed Roni on 2023-07-28.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
